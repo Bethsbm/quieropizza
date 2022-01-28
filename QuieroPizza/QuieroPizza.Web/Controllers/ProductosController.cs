044dab3fa4010fd7f136b41837979de48c082cf2
Maritza Sosa <maritza_sosa@live.com>
2022-01-28 15:49:25 -0600

    Creando Lista de Productos

---
+++ QuieroPizza/QuieroPizza.Web/Controllers/ProductosController.cs
@@ -0,0 +1,40 @@
+﻿using QuieroPizza.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuieroPizza.Web.Controllers
+{
+    public class ProductosController : Controller
+    {
+        // GET: Productos
+        public ActionResult Index()
+        {
+            var producto1 = new ProductoModel();
+            producto1.Id = 1;
+            producto1.Descripcion = "Pizza de pepperoni";
+
+            var producto2 = new ProductoModel();
+            producto2.Id = 2;
+            producto2.Descripcion = "Pizza vegetariana";
+
+            var producto3 = new ProductoModel();
+            producto3.Id = 3;
+            producto3.Descripcion = "Pizza gigante de queso";
+
+            var producto4 = new ProductoModel();
+            producto4.Id = 4;
+            producto4.Descripcion = "Pizza de jamón";
+
+            var listaProductos = new List<ProductoModel>();
+            listaProductos.Add(producto1);
+            listaProductos.Add(producto2);
+            listaProductos.Add(producto3);
+            listaProductos.Add(producto4);
+
+            return View(listaProductos);
+        }
+    }
+}
